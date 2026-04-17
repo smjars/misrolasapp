523ecc43cbef3e7ae19a2434bce81e2d6958f181
SMJARS <smjars@hotmail.com>
2026-04-17 21:38:20 +0200

    ui: se crea core\theme\theme.dart app_pallete.dart y signup_page.dart

---
+++ client/lib/core/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+  );
+}
